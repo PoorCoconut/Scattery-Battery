c539ced25b3d9611ba140da9843a9df870f49b44
Kyushey <cueshealforque@gmail.com>
2026-08-18 03:10:06 +0800

    file system clean-up

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -1,1 +1,41 @@
 extends CharacterBody2D
+class_name Player
+
+#Components
+@onready var health_component: HealthComponent = $HealthComponent
+@onready var hitbox_component: HitboxComponent = $HitboxComponent
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
+
+@export var SPEED : float = 100
+@export var ACCELERATION : float = 50
+@export var FRICTION : float = 5
+@export var MAX_TIME : float = 1.5
+@export var MAX_DISTANCE : float = 400.0
+
+var time_held : float = 0.0
+var distance_to : float = 0.0
+var thrusting : bool = false
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	look_at(get_global_mouse_position())
+	
+	var mouse_dir = (get_global_mouse_position() - global_position).normalized()
+	var can_still_thrust = time_held < MAX_TIME and distance_to < MAX_DISTANCE
+	
+	if Input.is_action_pressed("ui_accept") and can_still_thrust:
+		thrusting = true
+		time_held += delta
+		
+		velocity = velocity.move_toward(mouse_dir * SPEED, ACCELERATION * delta)
+		distance_to += velocity.length() * delta
+	else:
+		thrusting = false
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	if not Input.is_action_pressed("ui_accept") and velocity.length() < 1.0:
+		time_held = 0.0
+		distance_to = 0
+	move_and_slide()
---
+++ systems/Entity Components/Hitbox and Hurtbox Components/HurtBox/hurtbox_component.gd
@@ -0,0 +1,38 @@
+extends Area2D
+class_name HurtboxComponent
+
+@export var health_component: HealthComponent
+@export var movement_component: MovementComponent
+@export var weight: float = 10.0
+@export var iframe : float = 0.3
+var is_invincible: bool = false
+
+signal knockback_received(direction: Vector2, force: float)
+func _on_area_entered(area: Area2D) -> void:
+	if is_invincible:
+		return
+	
+	if area is HitboxComponent:
+		is_invincible = true
+		var hitbox = area as HitboxComponent
+		if health_component:
+			#print(get_parent().name + " hit for " + str(hitbox.damage) + " damage")
+			health_component.take_damage(hitbox.damage)
+		
+		var knockback_dir = (global_position - hitbox.global_position).normalized()
+		var final_knockback = max(0.0, hitbox.knockback_force - weight)
+		
+		if movement_component:
+			movement_component.apply_knockback(knockback_dir * final_knockback)
+		
+		knockback_received.emit(knockback_dir, final_knockback)
+		
+		hitbox.emit_hit_landed(global_position)
+		set_deferred("monitorable", false)
+		set_deferred("monitoring", false)
+		await get_tree().create_timer(iframe).timeout
+		if not is_instance_valid(self) or not is_inside_tree():
+			return
+		set_deferred("monitorable", true)
+		set_deferred("monitoring", true)
+		is_invincible = false
